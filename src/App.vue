2f031f87dbd32354c3d5ec83438a471870766825
Guillermo Verón <guiver95@gmail.com>
2022-07-25 16:10:41 -0400

    first practice with vue3 composition api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ greeting }}</h1>
+  <p>Capacity: {{ capacity }}</p>
+  <p>Spaces Left: {{ spacesLeft }}</p>
+  <p>Current Participants:</p>
+  <ul>
+    <li v-for="participant in participants" :key="participant">
+      {{ participant }}
+    </li>
+  </ul>
+  <button @click="increaseCapacity()">Increase Capacity</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, reactive, toRefs } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  setup() {
+    const data = reactive({
+      greeting: "Hello Composition API",
+      participants: ["Jhon", "Peter", "Chloe", "Michael"],
+      capacity: 5,
+      spacesLeft: computed(() => {
+        return data.capacity - data.participants.length;
+      }),
+    });
+
+    function increaseCapacity() {
+      data.capacity = data.capacity + 1;
+    }
+
+    return { ...toRefs(data), increaseCapacity };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
